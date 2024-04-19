190329487e53417dd334269975e7c9c7a40635da
Kardi <kgland06@gmail.com>
2024-04-19 10:48:00 +0700

    authentication

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,14 @@
 import { createStore } from "vuex";
 import coaachModule from "./modules/coaches/index";
 import requestModule from "./modules/requests/index";
+import authModule from "./modules/auth/index"
 
 const store = createStore({
     modules:{
         coaches:coaachModule,
-        requests:requestModule
+        requests:requestModule,
+        auth:authModule
     },
-    state(){
-        return {
-            userId:'c3'
-        }
-    },
-    getters:{
-        userId(state){
-            return state.userId;
-        }
-    }
 });
 
 export default store;
